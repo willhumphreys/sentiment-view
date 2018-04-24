dde5af9a3fcfd471e8d89e447a23d110bf214525
Will Humphreys <whumphreys@gmail.com>
2018-04-24 16:49:41 +0200

    Tidy up the initial page a bit more

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-    <h1>
-        <a href="https://s3.amazonaws.com/sentiment-plots/2018-04-24_04%3A04%3A32/aud-usd.png">aud-usd</a>
-    </h1>,
+    <div>
+    <h3>Currencies</h3>
+    <ul>
+       <li><a href="https://s3.amazonaws.com/sentiment-plots/2018-04-24_04%3A04%3A32/aud-usd.png">aud-usd</a></li>
+    </ul>
+    </div>,
     document.getElementById('root')
   );
