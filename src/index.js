52734bde7ffb06588fc8f57ae3866fd011e1e8be
Will Humphreys <whumphreys@gmail.com>
2018-04-24 16:45:52 +0200

    Add initial link

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 ReactDOM.render(
     <h1>
-        <p>Links</p>
+        <a href="https://s3.amazonaws.com/sentiment-plots/2018-04-24_04%3A04%3A32/aud-usd.png">aud-usd</a>
     </h1>,
     document.getElementById('root')
   );
