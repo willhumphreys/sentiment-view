24e3ad73e7546bceeaffb1f52758af9f47f787ae
Will Humphreys <whumphreys@gmail.com>
2018-04-26 14:40:39 +0200

    Add the other symbols

--- src/index.js
+++ src/index.js
@@ -2,20 +2,49 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function Symbol(props) {
-    return <li><a href={props.url}>{props.name}</a></li>
-
+    return <li><a href={`https://s3.amazonaws.com/sentiment-plots/2018-04-24_04%3A04%3A32/${props.value}.png`}>{props.value}</a></li>
 }
 
 function App() {
+
+    const currencies = ['aud-usd', 'eur-gbp', 'eur-jpy', 'eur-usd', 'gbp-usd', ,'nzd-usd', 'usd-cad', 'usd-jpy' ];
+
+    const currencyList = currencies.map((symbol) =>
+    <li><Symbol value={symbol}/></li>
+    );
+    
+    const commodities = ['gold', 'silver'];
+
+    const commodityList = commodities.map((symbol) =>
+    <li><Symbol value={symbol}/></li>
+    );
+
+    const indexes = ['wall-street'];
+
+    const indexList = indexes.map((symbol) =>
+    <li><Symbol value={symbol}/></li>
+    );
+
+   
     return (
         <div>
+            <h3>Currencies</h3>
             <ul>
-                <Symbol url="https://s3.amazonaws.com/sentiment-plots/2018-04-24_04%3A04%3A32/aud-usd.png" name="aud-usd" />
-                <Symbol url="https://s3.amazonaws.com/sentiment-plots/2018-04-24_04%3A04%3A32/eur-gbp.png" name="eur-gbp" />
+                {currencyList}
+            </ul>
+            <h3>Commodities</h3>
+            <ul>
+                {commodityList}
+            </ul>
+            <h3>Indexes</h3>
+            <ul>
+                {indexList}
             </ul>
         </div>
     );
 }
+
+
 
 
 ReactDOM.render(
