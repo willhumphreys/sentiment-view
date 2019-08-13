cef753cceadbbc1dbcc1c9878c8ebb1a69e0cb10
Will Humphreys <whumphreys@gmail.com>
2019-08-13 17:46:27 +0200

    Add more stocks from darwinex

--- src/index.js
+++ src/index.js
@@ -7,7 +7,14 @@ function Symbol(props) {
 
 const API = 'https://hi8hj4u1y1.execute-api.us-east-1.amazonaws.com/prod/currentpicks';
 const currencies = ['aud-usd', 'eur-gbp', 'eur-jpy', 'eur-usd', 'gbp-usd', 'nzd-usd', 'usd-cad', 'usd-jpy', 'usd-chf'];
-const stocks = ['GOOGL-US', 'AMZN-US'];
+const stocks = ["MMM-US","AXP-US","AAPL-US","BA-US","CAT-US","CVX-US","CSCO-US","KO-US","XOM-US","GE-US","GS-US",
+    "HD-US","IBM-US","INTC-US","JNJ-US","JPM-US","MCD-US","MRK-US","MSFT-US","NKE-US","PFE-US","PG-US","TRV-US",
+    "UNH-US","UTX-US","VZ-US","V-US","WMT-US","DIS-US","ATVI-US","ADBE-US","ALXN-US","GOOGL-US","GOOG-US","AMZN-US",
+    "AAL-US","AMGN-US","ADI-US","AAPL-US","AMAT-US","ADSK-US","ADP-US","AVGO-US","BIIB-US","CHTR-US","CSCO-US",
+    "CTSH-US","CMCSA-US","COST-US","CSX-US","DLTR-US","EBAY-US","EXPE-US","FB-US","FAST-US","GILD-US","ILMN-US",
+    "INTC-US","INTU-US","ISRG-US","LRCX-US","MAR-US","MCHP-US","MU-US","MSFT-US","NTAP-US","NFLX-US","NVDA-US",
+    "ORLY-US","QCOM-US","REGN-US","ROST-US","SWKS-US","SBUX-US","TSLA-US","TXN-US","FOXA-US","FOX-US","ULTA-US",
+    "VRTX-US","WDC-US"];
 
 const currencyList = currencies.map((symbol) =>
     <li><Symbol value={symbol}/></li>
